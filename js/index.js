a3422919affd787004e89b2db47e7c571ad9b0c9
nikkinbird <nikki.bollman@gmail.com>
2021-06-16 19:01:04 -0500

    created displayUsers function expression

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api?results=5");
+    const data = await usersRequest.json();
+    const usersResults = data.results;
+    console.log(usersResults);
+}
+
+const displayUsers = function (usersResults) {
+    randomFolks.innerHTML = "";
+}
+
